c25b2377b14c0cbace6828b6c484fdfd7cdb6c3c
Thijs Wiefferink <thijs@wiefferink.me>
2017-06-05 02:36:00 +0200

    Setup full color pallet for the Material-UI components

--- src/components/SiteConfiguration.tsx
+++ src/components/SiteConfiguration.tsx
@@ -1,5 +1,4 @@
 import {SiteContent} from 'components/SiteContent'
-import {green700} from 'material-ui/styles/colors'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import React from 'react'
@@ -17,7 +16,16 @@ export function SiteConfiguration() {
                 <MuiThemeProvider
                     muiTheme={getMuiTheme({
                         palette: {
-                            primary1Color: green700,
+                            primary1Color: '#388e3c',
+                            primary2Color: '#66bb6a',
+                            accent1Color: '#fb8c00',
+                            accent3Color: '#ff9800',
+                            primary3Color: '#e0e0e0',
+                            textColor: 'rgba(0, 0, 0, 0.9)',
+                            secondaryTextColor: 'rgba(0, 0, 0, 0.59)',
+                            borderColor: '#bdbdbd',
+                            pickerHeaderColor: '#388e3c',
+                            clockCircleColor: 'rgba(0, 0, 0, 0.05)',
                         },
                     })}
                 >
