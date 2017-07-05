b2807c7394b69374e1354e87de80974ddce08842
Thijs Wiefferink <thijs@wiefferink.me>
2017-07-05 21:25:44 +0200

    Make background fullscreen and 0.5 opacity
    Let me know if you like this better or not

--- src/modules/options/components/PageBackground.tsx
+++ src/modules/options/components/PageBackground.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import {connect} from 'react-redux'
 import {AppState} from 'reducer'
 
-// TODO use a pretty loading indicator and animation
 export function PageBackgroundDisplay({background}: StateToProps) {
     return (
         <div style={{
@@ -14,34 +13,12 @@ export function PageBackgroundDisplay({background}: StateToProps) {
             pointerEvents: 'none',
             overflow: 'hidden',
             backgroundColor: '#EEE',
-        }}>
-            <div style={{
-                width: '100%',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-            }}>
-                <img
-                    src={background}
-                    style={{
-                        width: '100%',
-                        height: 'auto',
-                    }}
-                />
-                <div style={{
-                    position: 'absolute',
-                    top: 0,
-                    right: 0,
-                    left: 0,
-                    bottom: 0,
-                    backgroundImage: 'linear-gradient(to bottom, rgba(238,238,238,0) 0%,rgba(238,238,238,0) 70%,rgba(238,238,238,1) 100%)',
-                    backgroundPosition: '50% 0',
-                    backgroundSize: '100%',
-                    backgroundRepeat: 'no-repeat',
-                }} />
-            </div>
-        </div>
+            backgroundImage: 'url('+background+')',
+            backgroundPosition: '50% 50%',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            opacity: 0.5,
+        }}/>
     )
 }
 
