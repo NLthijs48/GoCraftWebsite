02df5128c9f01ca65151aafe7968a684f4a9e05f
Thijs Wiefferink <thijs@wiefferink.me>
2017-07-24 16:00:33 +0200

    Filter ark players that are stuck in connecting state

--- src/modules/players/reducer.ts
+++ src/modules/players/reducer.ts
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux'
 import {get} from 'utils/utils'
 import * as t from './actionTypes'
-import {ArkPlayers, MinecraftPlayers} from './model'
+import {ArkPlayer, ArkPlayers, MinecraftPlayers} from './model'
 
 // Minecraft players
 export function minecraft(state: MinecraftPlayers = {}, action: t.PlayersAction): MinecraftPlayers {
@@ -17,13 +17,14 @@ export function minecraft(state: MinecraftPlayers = {}, action: t.PlayersAction)
 export function ark(state: ArkPlayers = {}, action: t.PlayersAction): ArkPlayers {
     switch(action.type) {
         case t.FETCH_ARK_SUCCESS:
+            // Filter players without name that are there for longer than 20 minutes (connecting bug?)
             return {
                 ...state,
                 default: (get(action.data, 'players')||[]).map((player: object) => ({
                     game: 'ark',
                     name: get(player, 'Name'),
                     time: get(player, 'Time'),
-                })),
+                })).filter((player: ArkPlayer) => player.name || player.time<60*20),
             }
         default:
             return state
