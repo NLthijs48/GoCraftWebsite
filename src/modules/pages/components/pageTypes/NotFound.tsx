75946ea84d0d1287fcbc5135f5f24c09cea46441
Thijs Wiefferink <thijs@wiefferink.me>
2018-02-17 15:02:18 +0100

    Improve 'Page not found' styling

--- src/modules/pages/components/pageTypes/NotFound.tsx
+++ src/modules/pages/components/pageTypes/NotFound.tsx
@@ -2,9 +2,20 @@ import * as React from 'react'
 
 export function NotFound() {
     return (
-        <div style={{textAlign: 'center'}}>
-            <div style={{fontSize: '30px', color: '#777', marginTop: '30px'}}>Page not found</div>
-            <div style={{marginTop: '15px', color: '#555'}}>Try going to another page using the menu</div>
+        <div style={{textAlign: 'center', padding: '1em'}}>
+            <h1 style={{
+                color: '#555',
+                marginTop: '30px',
+            }}>
+                Page not found
+            </h1>
+
+            <h2 style={{
+                marginTop: '15px',
+                color: '#999',
+            }}>
+                Try going to another page using the menu
+            </h2>
         </div>
     )
 }
