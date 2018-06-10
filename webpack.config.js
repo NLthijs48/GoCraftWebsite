f44cdaada15199b74ba9cd3c37d45ac595769a4d
Thijs Wiefferink <thijs@wiefferink.me>
2018-06-10 19:29:58 +0200

    Fix compile error

--- webpack.config.js
+++ webpack.config.js
@@ -86,48 +86,48 @@ module.exports = function (env) {
     const merged = Merge(commonConfig, environmentConfig);
     merged.plugins.push(
         // ServiceWorker and AppCache for offline use (should be last...)
-        new OfflinePlugin({
-            responseStrategy: 'cache-first',
-            appShell: '/',
-            updateStrategy: 'changed',
-            autoUpdate: 1000 * 60 * 5,
-            safeToUseOptionalCaches: true,
-            caches: {
-                // Entries are resolved as globs against all webpack assets
-                main: [':rest:'],
-                additional: [],
-                optional: [
-                    // Only cache the file types the browser loads
-                    'fonts/**',
-                    // Wordpress images
-                    /https:\/\/.*?\/wp-content\/uploads\/.*/,
-                ],
-            },
-            excludes: [
-                '**/.*',
-                '**/*.map',
-                '**/*.gz',
-                '**/wp-json/**',
-                '**/sw.js',
-                '**/subdomains/**',
-            ],
-            ServiceWorker: {
-                events: true,
-                navigationPreload: {
-                    test: (url) => {
-                        if(url.hostname !== 'go-craft.com') {
-                            console.log('SW: not caching because of hostname:', url);
-                            return false;
-                        }
-                        if(url.pathname && url.pathname.indexOf('/subdomains') === 0 && url.pathname.indexOf('/subdomains/wordpress/wp-content') !== 0) {
-                            console.log('SW: not caching because it is a subdomain:', url);
-                            return false;
-                        }
-                        return true;
-                    }
-                }
-            }
-        })
+        //new OfflinePlugin({
+        //    responseStrategy: 'cache-first',
+        //    appShell: '/',
+        //    updateStrategy: 'changed',
+        //    autoUpdate: 1000 * 60 * 5,
+        //    safeToUseOptionalCaches: true,
+        //    caches: {
+        //        // Entries are resolved as globs against all webpack assets
+        //        main: [':rest:'],
+        //        additional: [],
+        //        optional: [
+        //            // Only cache the file types the browser loads
+        //            'fonts/**',
+        //            // Wordpress images
+        //            /https:\/\/.*?\/wp-content\/uploads\/(.*)/,
+        //        ],
+        //    },
+        //    excludes: [
+        //        '**/.*',
+        //        '**/*.map',
+        //        '**/*.gz',
+        //        '**/wp-json/**',
+        //        '**/sw.js',
+        //        '**/subdomains/**',
+        //    ],
+        //    ServiceWorker: {
+        //        events: true,
+        //        navigationPreload: {
+        //            test: (url) => {
+        //                if(url.hostname !== 'go-craft.com') {
+        //                    console.log('SW: not caching because of hostname:', url);
+        //                    return false;
+        //                }
+        //                if(url.pathname && url.pathname.indexOf('/subdomains') === 0 && url.pathname.indexOf('/subdomains/wordpress/wp-content') !== 0) {
+        //                    console.log('SW: not caching because it is a subdomain:', url);
+        //                    return false;
+        //                }
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //})
     );
     return merged;
 };
